9cbf36a3c367fb7ef1ef6bcf4c41840c8b94f0db
Yash Kank <yashkank1999@gmail.com>
2024-10-09 13:18:48 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import ClientViewSet, ProjectViewSet
+
+router = DefaultRouter()
+router.register(r'clients', ClientViewSet)
+router.register(r'projects', ProjectViewSet)
+
+urlpatterns = router.urls
+
