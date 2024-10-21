6127d3cc791fe1cf7e4972aea36457d9cb54fb8c
Yash Kank <yashkank1999@gmail.com>
2024-10-21 23:09:33 +0530

    get all project list accordingly user id

--- api/views.py
+++ api/views.py
@@ -21,3 +21,34 @@ class ProjectViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
+from rest_framework import viewsets  
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from .models import Client, Project
+from .serializers import ClientSerializer, ProjectSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    @action(detail=True, methods=['get'], url_path='user-projects')
+    def user_projects(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        projects = Project.objects.filter(users=user)
+        serializer = self.get_serializer(projects, many=True)
+        return Response(serializer.data)
