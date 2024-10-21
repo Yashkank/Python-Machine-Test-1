d9c2be06f5b537f38aa804664941a384d1d7cbbe
Yash Kank <yashkank1999@gmail.com>
2024-10-21 23:30:46 +0530

    allow visibility of all projects assigned to a user

--- api/views.py
+++ api/views.py
@@ -1,26 +1,3 @@
-from rest_framework import viewsets
-from .models import Client, Project
-from .serializers import ClientSerializer, ProjectSerializer
-from rest_framework.permissions import IsAuthenticated
-
-class ClientViewSet(viewsets.ModelViewSet):
-    queryset = Client.objects.all()
-    serializer_class = ClientSerializer
-    permission_classes = [IsAuthenticated]
-    
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
-    
-    
-    
-class ProjectViewSet(viewsets.ModelViewSet):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
-
 from rest_framework import viewsets  
 from rest_framework.decorators import action
 from rest_framework.response import Response
